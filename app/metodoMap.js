90e2596e7eab29cfe0809dbcc188e396f7ba1198
Lucas Tsuji <lucas.tsuji@gmail.com>
2024-02-29 15:21:31 -0300

    final do evento

--- app/metodoMap.js
+++ app/metodoMap.js
@@ -1,8 +1,7 @@
 function aplicarDesconto(livros) {
     const desconto = 0.3
-    livrosComDesconto = livros.map(livro =>{
-        return {...livro, preço: livro.preco - (livro.preco * desconto)}
-
+    livrosComDesconto = livros.map(livro => {
+        return {...livro, preco: livro.preco - (livro.preco * desconto)}
     })
     return livrosComDesconto
 }
